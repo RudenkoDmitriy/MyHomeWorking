b9d65cf1437f3cbf90814e88925c20994f49fff6
RudenkoDmitriy <d.rudenko95@mail.ru>
2014-03-26 12:38:29 +0400

    Add control work.

---
+++ 2_semestr/KR/KR/TicTacToe.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KR
+{
+    public partial class TicTacToe : Form
+    {
+        private XO matrix;
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+            matrix = new XO();
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button ourButton = sender as Button;
+            if (ourButton.TabIndex < 3)
+            {
+                ourButton.Text = matrix.Add(0, ourButton.TabIndex % 3);
+                if (matrix.Result())
+                {
+                    MessageBox.Show("Winner is " + matrix.lastDownKey);
+                    clearAllKeys();
+                    matrix.Clear();
+                }
+                if (matrix.numberOfEnterKeys == 9)
+                {
+                    MessageBox.Show("Null result");
+                    clearAllKeys();
+                    matrix.Clear();
+                }
+            }
+            else if (ourButton.TabIndex < 6)
+            {
+                ourButton.Text = matrix.Add(1, ourButton.TabIndex % 3);
+                if (matrix.Result())
+                {
+                    MessageBox.Show("Winner is " + matrix.lastDownKey);
+                    clearAllKeys();
+                    matrix.Clear();
+                }
+                if (matrix.numberOfEnterKeys == 9)
+                {
+                    MessageBox.Show("Null result");
+                    clearAllKeys();
+                    matrix.Clear();
+                }
+            }
+            else
+            {
+                ourButton.Text = matrix.Add(2, ourButton.TabIndex % 3);
+                if (matrix.Result())
+                {
+                    MessageBox.Show("Winner is " + matrix.lastDownKey);
+                    clearAllKeys();
+                    matrix.Clear();
+                }
+                if (matrix.numberOfEnterKeys == 9)
+                {
+                    MessageBox.Show("Null result");
+                    clearAllKeys();
+                    matrix.Clear();
+                }
+            }
+        }
+        public void clearAllKeys()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+    }
+}
---
+++ 2_semestr/KR/XOTest/XOTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KR;
+
+namespace XOTest
+{
+    [TestClass]
+    public class XOTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            matrixOfGame = new XO();
+        }
+
+        [TestMethod]
+        public void ResultTest()
+        {
+            Assert.IsFalse(matrixOfGame.Result());
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            matrixOfGame.Add(0, 0);
+            matrixOfGame.Add(1, 1);
+            matrixOfGame.Add(0, 1);
+            matrixOfGame.Add(1, 2);
+            matrixOfGame.Add(0, 2);
+            Assert.IsTrue(matrixOfGame.Result());
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            matrixOfGame.Add(0, 0);
+            matrixOfGame.Add(1, 1);
+            matrixOfGame.Add(0, 1);
+            matrixOfGame.Add(1, 2);
+            matrixOfGame.Add(0, 2);
+            Assert.IsTrue(matrixOfGame.numberOfEnterKeys == 5);
+            matrixOfGame.Clear();
+            Assert.IsFalse(matrixOfGame.Result());
+            Assert.IsTrue(matrixOfGame.numberOfEnterKeys == 0);
+        }
+
+        private XO matrixOfGame;
+    }
+}
