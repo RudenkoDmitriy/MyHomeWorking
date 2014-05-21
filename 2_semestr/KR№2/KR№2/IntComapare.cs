28cc3387d0f128c74593204347e5d4189375814b
RudenkoDmitriy <d.rudenko95@mail.ru>
2014-05-21 12:43:45 +0400

    Add comments

--- 2_semestr/KR№2/KR№2/IntComapare.cs
+++ 2_semestr/KR№2/KR№2/IntComapare.cs
@@ -11,6 +11,12 @@ namespace KR_2
     /// </summary>
     public class IntComapare : IComparator<int>
     {
+        /// <summary>
+        /// Compare two int numbers.
+        /// </summary>
+        /// <param name="firstElement"></param>
+        /// <param name="secondElement"></param>
+        /// <returns></returns>
         public bool CompareTo(int firstElement, int secondElement)
         {
             return firstElement <= secondElement;
--- 2_semestr/KR№2/KR№2/StringCompare.cs
+++ 2_semestr/KR№2/KR№2/StringCompare.cs
@@ -11,6 +11,12 @@ namespace KR_2
     /// </summary>
     public class StringCompare : IComparator<string>
     {
+        /// <summary>
+        /// Compare two strings.
+        /// </summary>
+        /// <param name="firstElement"></param>
+        /// <param name="secondElement"></param>
+        /// <returns></returns>
         public bool CompareTo(string firstElement, string secondElement)
         {
             if (String.Compare(firstElement, secondElement) <= 0)
