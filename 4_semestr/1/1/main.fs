621c37b2775c4f9513165095bdbb6d0ccd903020
RudenkoDmitriy <d.rudenko95@gmail.com>
2015-02-16 19:47:59 +0300

    Add first homework

---
+++ 4_semestr/1/1/main.fs
@@ -0,0 +1,18 @@
+﻿let rec fact n = 
+  if n = 0 then 1
+  else n * fact (n - 1)
+
+let rec fib n = 
+  if n = 1 then 0
+  elif n < 3 then 1
+  else fib (n - 1) + fib (n - 2)
+
+let x = fib 4
+
+let y = fact 10
+
+printfn "%A" x
+
+printfn "%A" y
+
+
