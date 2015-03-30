7fae52f9b99b95035fa26436c5ad884cd0785582
RudenkoDmitriy <d.rudenko95@gmail.com>
2015-03-31 00:05:02 +0300

    Add sixth homework

---
+++ 4_semestr/6/6/Program.fs
@@ -0,0 +1,9 @@
+﻿type rounding(x : int) =
+    member t.Bind ((y : float), (rest: float -> float)) = rest <| System.Math.Round(y, x)
+    member t.Return(y : float) = System.Math.Round(y, x)
+
+let x = rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
