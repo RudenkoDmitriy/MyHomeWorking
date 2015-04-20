4c77e9cd94df6293a26073788f108671121b07de
RudenkoDmitriy <d.rudenko95@gmail.com>
2015-04-20 15:05:52 +0300

    Rewrite KR

---
+++ 4_semestr/RewriteKR/RewriteKR/Program.fs
@@ -0,0 +1,14 @@
+﻿//1
+let supermap list func =
+    let rec helpFunc ((list : 'a list), (func : 'a -> 'b list), (acc : 'b list)) =
+        match list with
+        |[] -> acc
+        |h::t -> helpFunc(t, func, acc @ func h)
+    helpFunc(list, func, [])
+
+printfn "%A" <| supermap [1; 2; 3] (fun x -> [x;x])
+
+  
+    
+
+    
