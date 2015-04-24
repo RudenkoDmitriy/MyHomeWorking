a54bf6bac8d3ab8be39518f7023706a143a89710
RudenkoDmitriy <d.rudenko95@gmail.com>
2015-04-24 15:08:01 +0300

    Add rewrite №2

---
+++ 4_semestr/RewriteKR№2/RewriteKR№2/Program.fs
@@ -0,0 +1,10 @@
+﻿/// 1
+let averageOfSinInList list =
+    let rec helpFunc list sum number =
+        match list with
+        |[] -> sum / number
+        |h::t -> helpFunc t (sum + sin h) (number + 1.0)
+    helpFunc list 0.0 0.0
+
+printfn "%A" << averageOfSinInList <| [1.0; 2.0; 3.0]
+
